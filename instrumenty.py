704ff3f3002dfbf12f4d863e80448219dcef1547
Ola <aleksandra.burchart@gmail.com>
2019-01-14 20:00:11 +0100

    podłączenie funkcji do przycisku 'Pobierz dane' - pobiera aktualne dane, oraz funkcje get_actual_value i get_actual_date zeby wywoływały się przy zmianie waluty

--- instrumenty.py
+++ instrumenty.py
@@ -2,7 +2,8 @@ from PyQt5 import QtWidgets, uic
 from PyQt5.QtWidgets import QMessageBox
 
 from Modelgui import model_licz
-
+from data.data_act import get_actual_value, get_actual_date
+from data.data_download import download_actual_gold, download_actual_currency
 
 app = QtWidgets.QApplication([])
 dlg = uic.loadUi("instrumenty.ui")
@@ -11,11 +12,14 @@ def show_message(title='Test', message='Test'):
     QMessageBox.information(None, title, message)
 
 def pobierz_dane():
+    download_actual_gold()
+    download_actual_currency()
     show_message('Info', "Dane zostały pobrane")
 
 def aktualny():
-    dlg.lineEdit.setText(waluty[dlg.comboBox.currentText()])
-    dlg.lineEdit_3.setText('tu trzeba podpiąć aktualną datę')
+    #dlg.lineEdit.setText(waluty[dlg.comboBox.currentText()])
+    dlg.lineEdit.setText(get_actual_value(dlg.comboBox.currentText()))
+    dlg.lineEdit_3.setText(get_actual_date(dlg.comboBox.currentText()))
 
 
 waluty = {'złoto': 'gold', 'euro': 'EUR', 'funt szterling': 'GBP', 'dolar amerykański': 'USD', 'korona czeska': 'CZK', 'peso meksykańskie': 'MXN', 'rubel rosyjski': 'RUB'}
@@ -28,6 +32,8 @@ def display_forecast():
 dlg.comboBox.currentIndexChanged.connect(aktualny)
 dlg.pushButton.clicked.connect(display_forecast)
 dlg.pushButton_2.clicked.connect(pobierz_dane)
+
+print(get_actual_value('gold'))
 
 dlg.show()
 app.exec()
